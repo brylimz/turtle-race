100840d3b2138fdc00b85bcb0e50c65bec7c1b0e
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-03 09:19:53 +0700

    finishing the project

--- day19.py
+++ day19.py
@@ -1,40 +1,66 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? enter a color:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-
-def move_forwards():
-    tim.forward(18)
-
-
-def move_backwards():
-    tim.backward(18)
-
-
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
-
-
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
-
-
-def clear():
-    tim.clear()
+print(user_bet)
+for turtle_index in range(0, 6):
+    tim = Turtle()
+    tim.shape("turtle")
+    tim.color(colors[turtle_index])
     tim.penup()
-    tim.home()
-    tim.pendown()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 
-screen.listen()
-screen.onkey(move_forwards, "w")
-screen.onkey(move_backwards, "s")
-screen.onkey(turn_left, "a")
-screen.onkey(turn_right, "d")
-screen.onkey(clear, "c")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def move_forwards():
+#     tim.forward(18)
+#
+#
+# def move_backwards():
+#     tim.backward(18)
+#
+#
+# def turn_left():
+#     new_heading = tim.heading() + 10
+#     tim.setheading(new_heading)
+#
+#
+# def turn_right():
+#     new_heading = tim.heading() - 10
+#     tim.setheading(new_heading)
+#
+#
+# def clear():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+#
+#
+# screen.listen()
+# screen.onkey(move_forwards, "w")
+# screen.onkey(move_backwards, "s")
+# screen.onkey(turn_left, "a")
+# screen.onkey(turn_right, "d")
+# screen.onkey(clear, "c")
 
 
 screen.exitonclick()
