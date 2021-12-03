efe2e695785bad758060c42ebbb1f16a74e3a16c
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-03 09:31:26 +0700

    well played

--- day19.py
+++ day19.py
@@ -25,8 +25,16 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-    rand_distance = random.randint(0, 10)
-    turtle.forward(rand_distance)
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! the {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! the {winning_color} turtle is the winner!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 
