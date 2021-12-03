1c8d5362f80396a460f5c55a5b32b8765760ba92
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-03 09:26:11 +0700

    well played

--- day19.py
+++ day19.py
@@ -9,14 +9,17 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? enter a color:")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
 
 print(user_bet)
 for turtle_index in range(0, 6):
-    tim = Turtle()
-    tim.shape("turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle()
+    new_turtle.shape("turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 if user_bet:
     is_race_on = True
